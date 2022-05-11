18eaeb605b3640e32c48d2ec00efe70a1818ff1c
Daniel Marzini <44803752+danielmarzini@users.noreply.github.com>
2022-05-11 09:51:58 +0200

    tests

---
+++ internal/limits/limits.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	v1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func main() {
+	ctx := context.Background()
+	config := ctrl.GetConfigOrDie()
+	clientset := kubernetes.NewForConfigOrDie(config)
+
+	namespace := "kube-system"
+	items, err := GetDeployments(clientset, ctx, namespace)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for _, item := range items {
+			fmt.Printf("%+v\n", item)
+		}
+	}
+}
+
+func GetDeployments(clientset *kubernetes.Clientset, ctx context.Context,
+	namespace string) ([]v1.Deployment, error) {
+
+	list, err := clientset.AppsV1().Deployments(namespace).
+		List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return list.Items, nil
+}
