7f10ee31588e0d4a0d035be3011f2f7087e863b6
Ricardo Godoy <rgodoy@google.com>
2022-10-24 11:51:02 -0300

    Adding -json flag to output results to stdout in JSON format

---
+++ internal/outputs/console_json_collector_test.go
@@ -0,0 +1,51 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package outputs
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/google/gke-policy-automation/internal/policy"
+)
+
+func TestConsoleJsonResultCollector(t *testing.T) {
+	var buff bytes.Buffer
+	var expectedJson = "json string"
+
+	out := &Output{w: &buff}
+	reportMapperMock := &validationReportMapperMock{
+		addResultsFn: func(results []*policy.PolicyEvaluationResult) {},
+		getJsonReportFn: func() ([]byte, error) {
+			return []byte(expectedJson), nil
+		},
+	}
+
+	collector := &consoleJsonResultCollector{out: out, reportMapper: reportMapperMock}
+	err := collector.RegisterResult([]*policy.PolicyEvaluationResult{{}})
+	if err != nil {
+		t.Fatalf("err on RegisterResult = %v; want nil", err)
+	}
+	err = collector.Close()
+	if err != nil {
+		t.Fatalf("err on Close = %v; want nil", err)
+	}
+	if len(buff.String()) <= 0 {
+		t.Errorf("nothing was written to the output buffer")
+	}
+	if buff.String() != expectedJson {
+		t.Errorf("expected \"%v\" but buffer was \"%v\"", expectedJson, buff.String())
+	}
+}
